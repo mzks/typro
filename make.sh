e62b9dd3331c274cc3750823c591c6ee5a43d53b
Keita Mizukoshi <mzks@stu.kobe-u.ac.jp>
2021-04-30 06:39:10 +0900

    Fix script

--- make.sh
+++ make.sh
@@ -7,7 +7,7 @@ python3 setup.py bdist_wheel
 
 echo Done.
 echo local install
-echo python3 -m pip install dist/typro-x.x.x-py3-none-any.whl --upgrade
+echo 'python3 -m pip install dist/typro-x.x.x-py3-none-any.whl --upgrade'
 
 echo upload pypi
-echo python3 -m twine upload --repository pypi dist/*
+echo 'python3 -m twine upload --repository pypi dist/*'
